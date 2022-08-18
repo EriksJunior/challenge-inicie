94879576b10c4ed2a6426653a9a9f0bade1e9354
eriks junior <eriksjunior13@gmail.com>
2022-08-18 20:26:14 -0300

    feat: finalizado testes(CommentsController)

--- __tests__/unit/Comments/CommentsController.test.ts
+++ __tests__/unit/Comments/CommentsController.test.ts
@@ -6,8 +6,14 @@ describe('Create a comments', () => {
   const mockPostData = {
     post_id: 1,
 		name: "test",
-		email: "trest@teste7657.com",
+		email: "trest@teste7658.com",
 		body: "testttttttttttt"
+  }
+
+  const mockPostDataFromFistPost = {
+		name: "test",
+		email: "trest@teste7658.com",
+		body: "test post public list"
   }
 
   it('Must be able to creates a new comment', async () => {
@@ -18,4 +24,35 @@ describe('Create a comments', () => {
     expect(res.status).toEqual(201)
     expect(res.body).toHaveProperty('id')
   })
+
+  
+  it('Must be able to creates a new comment in the first post in list public', async () => {
+    const res = await request(app)
+      .post('/comments/createcommentinfistpostpublic')
+      .send(mockPostDataFromFistPost)
+
+    expect(res.status).toEqual(201)
+    expect(res.body).toHaveProperty('id')
+  })
+
+  it('Must be able to find comments by public post id', async () => {
+    const idPost = 1
+
+    const res = await request(app) 
+      .get(`/comments/findcommentsbypublicpostid/${idPost}`)
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toEqual(expect.arrayContaining([]))
+  })
+  
+   it('Must be able to delete comment', async () => {
+    const idComment = 1928
+
+    const res = await request(app) 
+      .delete(`/comments/deletecomment/${idComment}`)
+
+    expect(res.status).toEqual(200)
+    expect(res.body).toHaveProperty('message')
+  })
+
 })
