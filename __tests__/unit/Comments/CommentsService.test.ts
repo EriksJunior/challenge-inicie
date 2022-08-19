31e27fcdadc5a284856e57f26db26181c624b3cd
eriks junior <eriksjunior13@gmail.com>
2022-08-18 21:02:30 -0300

    feat: finalizado teste(CommentsService)

--- __tests__/unit/Comments/CommentsService.test.ts
+++ __tests__/unit/Comments/CommentsService.test.ts
@@ -14,10 +14,36 @@ describe("Create a comments", () => {
     body: "testttttttttttt"
   }
 
+  const mockPostDataFromFistPost: CommentsEntity = {
+    name: "test",
+    email: "trest@1.com",
+    body: "test post public list"
+  }
+
   it('Must be able to creates a new comment', async () => {
     const id = await commentsService.createCommentOnPost(mockCommentData)
 
     expect(id).toHaveProperty('id')
+  })
+
+  it('Must be able to creates a new comment in the first post in list public', async () => {
+    const id = await commentsService.createCommentTheFirstPostInListPublic(mockPostDataFromFistPost)
+
+    expect(id).toHaveProperty('id')
+  })
+
+  it('Must be able to find comments by public post id', async () => {
+    const publicPosts = await commentsService.findCommentsByPublicPostId(mockCommentData.post_id as number)
+
+    expect(publicPosts).toEqual(expect.arrayContaining([]))
+  })
+
+  it('Must be able to delete comment by id', async () => {
+    const id = 1928
+
+    const statusCode = await commentsService.deleteComment(id)
+
+    expect(statusCode.status).toEqual(204)
   })
 
 })
